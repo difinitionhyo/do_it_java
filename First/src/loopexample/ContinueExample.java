ccc36c9bf4fbf7af3206fa0a5767ca818831caec
difinitionhyo <definitionhyo@gmail.com>
2023-03-10 17:37:01 +0900

    0310

---
+++ First/src/loopexample/ContinueExample.java
@@ -0,0 +1,28 @@
+package loopexample;
+
+public class ContinueExample {
+
+	public static void main(String[] args) {
+		// 1~100더할때 홀수일떄만더하기
+		
+		int total = 0;
+		for(int i = 1; i < 101; i++) {
+			if(i % 2 == 0) 
+				continue;
+				total += i;
+			    
+		}
+		System.out.println("1~100까지주 홀수만 더한 값 : " + total);
+
+		
+		// 1~100까지 수 중 3의배수만 출력
+		
+		for(int j= 1; j < 101; j++) {
+			if (j % 3 != 0)
+				continue;
+			System.out.println(j);
+		}
+		
+	}
+
+}
